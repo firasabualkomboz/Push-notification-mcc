4b61a72e99b165ad4a6dd54972ade971d3611a59
firasabualkomboz <firasa018@gmail.com>
2021-05-10 21:57:08 +0300

    ss

---
+++ app/src/main/java/com/example/push_notification/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.push_notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            setContentView(R.layout.layout_sign_in);
+
+        }, 2000)
+
+
+    }
+}
